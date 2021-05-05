21c4099ca79848a7c2e89ae2cce6867d2bba0db3
fajariadi-id <fajariadi.id@gmail.com>
2021-05-05 13:14:13 +0700

    pulled by fajar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import SignIn from './hanendyo/SignIn/SignIn';
 import SignUp from './hanendyo/SignUp/SignUp';
-import Footer from './master/components/Footer/Footer';
-import Navbar from './master/components/Navbar/Navbar';
-import NavbarLandingPage from './master/components/Navbar/NavbarLandingPage';
 
 const App = () => {
   return (
     <div>
-      <SignUp/>
-      <SignIn/>
-      {/* <NavbarLandingPage />
-      <h1>Content</h1>
-      <Navbar />
-      <Footer /> */}
+      <SignUp />
+      <SignIn />
     </div>
   );
 };
