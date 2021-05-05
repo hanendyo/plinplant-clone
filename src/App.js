cd11a59d40b0257720c00e71869030858e0d69c4
fajariadi-id <fajariadi.id@gmail.com>
2021-05-05 12:31:24 +0700

    pulled by fajar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import Ensiklopedia from "./dhika/Ensiklopedia";
-import React from "react";
-import Footer from "./master/components/Footer/Footer";
-import Navbar from "./master/components/Navbar/Navbar";
-import NavbarLandingPage from "./master/components/Navbar/NavbarLandingPage";
+import Ensiklopedia from './dhika/Ensiklopedia';
+import React from 'react';
+import Footer from './master/components/Footer/Footer';
+import Navbar from './master/components/Navbar/Navbar';
 
 const App = () => {
   return (
