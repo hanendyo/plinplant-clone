9a6f00c144f2437d5a91b3a158069e2e4cbf822b
dhikadhiwijna <adiwijna02@gmail.com>
2021-05-05 20:46:19 +0700

    Artikel kurang bagian "Artikel lain"

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import Footer from "./master/components/Footer/Footer";
 import Navbar from "./master/components/Navbar/Navbar";
 import NavbarLandingPage from "./master/components/Navbar/NavbarLandingPage";
 import Ensiklopedia from "./dhika/Ensiklopedia/Ensiklopedia";
-import Artikel from "./dhika/Artikel/Artikel";
+import NewsHeader from "./dhika/Artikel/header/ArtikelHeader";
 
 const App = () => {
   return (
     <div>
       <Navbar />
-      {/* <Ensiklopedia /> */}
-      <Artikel />
+      <Ensiklopedia />
+      {/* <NewsHeader /> */}
       <Footer />
     </div>
   );
