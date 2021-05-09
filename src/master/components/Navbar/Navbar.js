2147034e3f5ae864a265b4c7917ddd04fbc1b21f
fajariadi-id <fajariadi.id@gmail.com>
2021-05-09 23:47:06 +0700

    progress hover profile

--- src/master/components/Navbar/Navbar.js
+++ src/master/components/Navbar/Navbar.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Home, Logo, Nav, LinksContainer, Container } from './Navbar.elemen';
 import { FaChevronLeft, FaShoppingCart } from 'react-icons/fa';
 import Button from '../additional/Button';
 import { colors } from '../../constant/style';
+import pic from '../../../fajariadi/assets/images/ig.jpg';
 
 const Navbar = () => {
+  const [login, setLogin] = useState(false);
+
   return (
     <Nav>
       <Container>
@@ -20,12 +23,41 @@ const Navbar = () => {
           </li>
           <li>Artikel</li>
           <li>
-            <Button text='Masuk' bgColor={colors.white} />
-            <Button
-              primary
-              text='Daftar'
-              bgColor={colors.lightGreenTransparent}
-            />
+            {login ? (
+              <>
+                <button id='profile'>
+                  <img src={pic} alt='' />
+                  <p>Halo, Fajar</p>
+                </button>
+
+                {/* Profile Hover */}
+                <div>
+                  <div>
+                    <img src={pic} alt='' />
+
+                    <div>
+                      <h5>Fajar Riadi</h5>
+                      <span>fajariadi.js@gmail.com</span>
+                    </div>
+                  </div>
+
+                  <ul>
+                    <li>Profil</li>
+                    <li>Daftar Transaksi</li>
+                    <li>Keluar</li>
+                  </ul>
+                </div>
+              </>
+            ) : (
+              <>
+                <Button text='Masuk' bgColor={colors.white} />
+                <Button
+                  primary
+                  text='Daftar'
+                  bgColor={colors.lightGreenTransparent}
+                />
+              </>
+            )}
           </li>
         </LinksContainer>
       </Container>
--- src/master/components/Navbar/NavbarLandingPage.js
+++ src/master/components/Navbar/NavbarLandingPage.js
@@ -1,27 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, LinksContainer, Logo, Nav } from './Navbar.elemen';
 import { FaShoppingCart } from 'react-icons/fa';
 import Button from '../additional/Button';
 import { colors } from '../../constant/style';
+import pic from '../../../fajariadi/assets/images/ig.jpg';
 
 const NavbarLandingPage = () => {
+  const [login, setLogin] = useState(false);
+
   return (
     <Nav>
       <Container>
         <Logo>PlinPlant</Logo>
 
-        <LinksContainer>
+        <LinksContainer login={login}>
           <li>
             <FaShoppingCart className='cart' />
           </li>
           <li>Artikel</li>
           <li>
-            <Button text='Masuk' bgColor={colors.white} />
-            <Button
-              primary
-              text='Daftar'
-              bgColor={colors.lightGreenTransparent}
-            />
+            {login ? (
+              <>
+                <button>
+                  <img src={pic} alt='' />
+                  <p>Halo, Fajar</p>
+                </button>
+
+                {/* Profile Hover */}
+                <div>
+                  <div>
+                    <img src={pic} alt='' />
+
+                    <div>
+                      <h5>Fajar Riadi</h5>
+                      <span>fajariadi.js@gmail.com</span>
+                    </div>
+                  </div>
+
+                  <ul>
+                    <li>Profil</li>
+                    <li>Daftar Transaksi</li>
+                    <li>Keluar</li>
+                  </ul>
+                </div>
+              </>
+            ) : (
+              <>
+                <Button
+                  text='Masuk'
+                  bgColor={colors.white}
+                  onClick={console.log('Masuk')}
+                />
+                <Button
+                  primary
+                  text='Daftar'
+                  bgColor={colors.lightGreenTransparent}
+                  onClick={console.log('Daftar')}
+                />
+              </>
+            )}
           </li>
         </LinksContainer>
       </Container>
