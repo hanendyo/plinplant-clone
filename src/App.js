cbf30ca2080d0b0b5b4c8a80995de8b59463b8ac
fajariadi-id <fajariadi.id@gmail.com>
2021-05-16 20:22:35 +0700

    merging ke fajar

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
-
-import SignIn from "./hanendyo/SignIn/SignIn";
-import SignUp from "./hanendyo/SignUp/SignUp";
-import Footer from "./master/components/Footer/Footer";
-import Navbar from "./master/components/Navbar/Navbar";
-import NavbarLandingPage from "./master/components/Navbar/NavbarLandingPage";
-
-import { ContextProvider } from "./context/store/ContextStore";
-import CMS from "./hanendyo/CMS/CMS";
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import SignIn from './hanendyo/SignIn/SignIn';
+import SignUp from './hanendyo/SignUp/SignUp';
+import { ContextProvider } from './context/store/ContextStore';
+import CMS from './hanendyo/CMS/CMS';
 
 const App = () => {
   return (
     <ContextProvider>
       <Router>
         <Switch>
-          <Route path='/register'><SignUp /></Route>
-          <Route path='/cms'><CMS /></Route>
+          <Route path='/register'>
+            <SignUp />
+          </Route>
+          <Route path='/login'>
+            <SignIn />
+          </Route>
+          <Route path='/cms'>
+            <CMS />
+          </Route>
         </Switch>
-        
-        {/* <SignIn/> */}
-        {/* <NavbarLandingPage />
-          <h1>Content</h1>
-          <Navbar />
-          <Footer /> */}
       </Router>
     </ContextProvider>
   );
