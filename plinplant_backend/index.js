cf2dc03401f323b5dab9aba5eaad30c5dd162417
Hanendyo Indira <hanendyo@Hanendyos-MacBook-Air.local>
2021-05-26 20:00:58 +0700

    editing BACKEND

--- plinplant_backend/index.js
+++ plinplant_backend/index.js
@@ -1,78 +1,67 @@
-const express = require('express');
-const cors = require('cors');
-const multer = require('multer');
+const express = require("express");
+const cors = require("cors");
+const multer = require("multer");
 const path = require('path');
-const dotenv = require('dotenv');
-const cookieParser = require('cookie-parser');
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
 
 // config
 const app = express();
 
 // file storage
 const fileStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    let fieldName = file.fieldname;
-    if (fieldName === 'article_image_upload') {
-      cb(null, '../public/images/article_image');
+  destination: (req, file, cb)=>{
+    let fieldName = file.fieldname
+    if(fieldName === 'article_image_upload') {
+      cb(null, './images/article_image');
     }
-    if (fieldName === 'plant_image_upload') {
-      cb(null, '../public/images/plant_image');
+    if(fieldName === 'plant_image_upload') {
+      cb(null, './images/plant_image');
     }
-    if (
-      fieldName === 'seed_image_upload' ||
-      fieldName === 'tuber_image_upload' ||
-      fieldName === 'young_image_upload' ||
-      fieldName === 'mature_image_upload'
-    ) {
-      cb(null, '../public/images/plant_breeding_image');
+    if(fieldName === 'seed_image_upload' || fieldName === 'tuber_image_upload' || fieldName === 'young_image_upload' || fieldName === 'mature_image_upload') {
+      cb(null, './images/plant_breeding_image');
     }
-    if (fieldName === 'picture_upload') {
-      cb(null, '../public/images/user_image');
+    if(fieldName === 'picture_upload'){
+      cb(null, './images/user_image');
     }
     console.log(`FILE DES: `, file);
+      
   },
-  filename: (req, file, cb) => {
-    const pathname = req.path;
-    cb(
-      null,
-      pathname + '_image_' + Date.now() + path.extname(file.originalname)
-    );
-  },
-});
-
-const checkFileType = (req, file, cb) => {
-  if (
-    file.mimetype === 'image/png' ||
-    file.mimetype === 'image/jpg' ||
-    file.mimetype === 'image/jpeg'
-  ) {
-    cb(null, true);
-  } else {
-    cb(null, false);
+  filename: (req, file, cb)=>{
+      const pathname = req.path
+      cb(null, pathname + '_image_' + Date.now() + path.extname(file.originalname))
   }
-};
-
+})
+const checkFileType = (req, file, cb) => {
+  if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
+    cb(null, true)
+  } else {
+    cb(null, false)
+  }
+}
 const upload = multer({
   storage: fileStorage,
-  limits: { fileSize: 5000000 },
-  fileFilter: checkFileType,
-});
+  limits: {fileSize: 5000000},
+  fileFilter: checkFileType
+})
 module.exports = upload;
+
 
 // .env
 dotenv.config();
 
-const authRoutes = require('./src/router/Auth_router');
-const plantRoutes = require('./src/router/Plant_router');
+const authRoutes = require("./src/router/Auth_router");
+const plantRoutes = require("./src/router/Plant_router");
 
 // middleware
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
+
 // path
-app.use('/auth', authRoutes);
-app.use('/input', plantRoutes);
+app.use("/auth", authRoutes);
+app.use("/input",plantRoutes);
 
 // Setup server
 const APP_PORT = process.env.APP_PORT;
